cf10b6e67be659ee55650285fbe1e4b1a20c839e
Sonu <un4@unnatihelpers.com>
2016-06-28 14:29:00 +0530

    authentication

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -3,17 +3,20 @@ class ToolsController < ApplicationController
   def check_caimpaign
   	file = params[:csv_file]
   	name = params[:name]
-  	if file.present?
-  		phones = TestCaimpaignStatus.import_csv(file)
-  		if phones.present?
-  			caimpaign = TestCaimpaign.create(name: name)
-  			if caimpaign.present?
-	  			counter = 1
-		  		TestCaimpaignStatus.delay(run_at: 10.seconds.from_now, queue: 'main').failed_phone_csv(phones, counter, caimpaign.id)
-		  		redirect_to download_test_caimpaign_path
-		  	end
-	  	end
-  	end
+    password = params[:password]
+    if password == "testcaimpaign@unnatihelpers"
+      if file.present?
+        phones = TestCaimpaignStatus.import_csv(file)
+        if phones.present?
+          caimpaign = TestCaimpaign.create(name: name)
+          if caimpaign.present?
+            counter = 1
+            TestCaimpaignStatus.delay(run_at: 10.seconds.from_now, queue: 'main').failed_phone_csv(phones, counter, caimpaign.id)
+            redirect_to download_test_caimpaign_path
+          end
+        end
+      end
+    end
   end
 
   def export
